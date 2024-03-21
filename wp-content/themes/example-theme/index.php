1d631a411a3fe4e70cba6ca8a516c31e4b3d9b66
liem nguyen <elminguyen@gmail.com>
2024-03-21 10:43:25 +0200

    test gitugnore

---
+++ wp-content/themes/example-theme/index.php
@@ -0,0 +1,11 @@
+<?php
+if ( have_posts() ) :
+  while ( have_posts() ) :
+    the_post();
+    the_title();
+    the_content();
+  endwhile;
+else :
+  _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+endif;
+?>
