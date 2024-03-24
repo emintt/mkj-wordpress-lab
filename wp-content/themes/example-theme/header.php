3175fe683f0bf3a4137842c53b053c2618c4462d
liem nguyen <elminguyen@gmail.com>
2024-03-24 22:49:08 +0200

    viikko 1 ready

---
+++ wp-content/themes/example-theme/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php wp_head(); ?>
+</head>
+
+<body>
+<div class="container">
+  <header class="page-header">
+    <div class="header-top-left">
+        <?php the_custom_logo(); ?>
+      <!-- <img src="//placehold.it/200x100?text=Logo" alt="Logo"> -->
+    </div>
+    <div class="header-top-right">
+      <?php wp_nav_menu([ 'container' => 'nav', 'theme_location' => 'main-menu']) ?>
+        <!-- <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">About us</a></li>
+          </ul>
+        </nav> -->
+    </div>
+  </header>
